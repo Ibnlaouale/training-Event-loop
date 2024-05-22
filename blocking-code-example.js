0ef6ca6b0eba18de46e865a5f8ae94ae9b3c4cf5
Ibnlaouale <dinilaouale@gmail.com>
2024-05-22 23:37:13 +0100

    comment line boucle while

--- blocking-code-example.js
+++ blocking-code-example.js
@@ -3,7 +3,7 @@ console.log('Début du script');
 // Opération bloquante simulant une grosse tâche synchrone
 const startTime = Date.now();
 while (Date.now() - startTime < 1000) {
-    console.log('blockage en cours');
+    // console.log('blockage en cours');
   // boucle pendant 1 seconde
 }
 
